47e6f7ca20ae2a2fe77791cfe8b85cf75df72ef1
Nathan Elorreaga <nathan.elorreaga@gmail.com>
2019-09-13 15:15:31 -0600

    changed file path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static("Public"));
 app.listen(PORT, function () {
     console.log("App listening on http://localhost:" + PORT);
  });
