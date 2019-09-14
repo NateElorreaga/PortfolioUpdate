46aaa3135d34ae735cb8c1176e60597df6bdfc02
Nathan Elorreaga <nathan.elorreaga@gmail.com>
2019-09-13 21:52:52 -0600

    cleaned up the page with styling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var path = require("path");
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var octicons = require("octicons")
+octicons.alert
 
 var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
